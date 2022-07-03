ed70050a769596fa02a4d9de8b7a62925bffe7c4
AniGulkanyan <gulkanyanani.1995@gmail.com>
2022-07-03 16:24:50 +0400

    remove odd code

--- src/modules/components/SearchBar/index.jsx
+++ src/modules/components/SearchBar/index.jsx
@@ -3,7 +3,7 @@ import TextField from '@mui/material/TextField';
 
 export const SearchBar = ({onChangeHandler}) => {
     return <TextField
-                onChange={(event) => onChangeHandler(event.target.value)}
+                onChange={(event) => onChangeHandler(event.target.value.trim())}
                 color="success"
                 label="name/surname/email"
                 margin="dense"
