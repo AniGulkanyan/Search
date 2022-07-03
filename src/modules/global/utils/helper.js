6ca6fd8f272b88e5a5f46ed990f779438ac96e36
AniGulkanyan <gulkanyanani.1995@gmail.com>
2022-07-04 01:59:24 +0400

    remove types

--- src/modules/global/utils/helper.js
+++ src/modules/global/utils/helper.js
@@ -1,6 +1,6 @@
-export const debounce = (func: Function, timeout = 300) => {
-    let timer: NodeJS.Timer;
-    return (...args: any) => {
+export const debounce = (func, timeout = 300) => {
+    let timer;
+    return (...args) => {
         clearTimeout(timer);
         timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
